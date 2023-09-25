9c7a8b3c944c09822e96b5b6d157aeb863ee5936
FelipeFabricio <ff.oliveira32@gmail.com>
2023-09-24 23:43:33 -0300

    Remove unnecessary comment

--- fileHandler/fileHandler.go
+++ fileHandler/fileHandler.go
@@ -45,9 +45,6 @@ func WriteFile(fileName string, texts []string) {
 	fmt.Println("Changes done successfully in " + fileName)
 }
 
-// Fprint formats using the default formats for its operands and writes to w.
-// Spaces are added between operands when neither is a string.
-// It returns the number of bytes written and any write error encountered.
 func ReadFile(fileName string) {
 	file, err := os.OpenFile(fileName, os.O_RDWR, 0644)
 	if err != nil {
